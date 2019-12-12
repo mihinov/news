85d1814e68cbb0495eb9fea5f365d608c700fedd
Kamahin Mihail <45814283+mihinov@users.noreply.github.com>
2019-12-12 13:15:12 +0300

    Add files via upload

--- js/header.js
+++ js/header.js
@@ -14,7 +14,7 @@ document.addEventListener('scroll', () => {
 let header__burger = document.querySelector('.header__burger');
 let burger__menu = document.querySelector('.burger__menu');
 let header__container = document.querySelector('.header__container');
-let container = document.querySelector('.container');
+let posts__intro = document.querySelector('.posts__intro');
 let header__logo = document.querySelector('.header__logo');
 
 function whatPhone() {
@@ -22,18 +22,25 @@ function whatPhone() {
 }
 
 function fuckCalc() {
-	let paddingContainer = window.getComputedStyle(container, null).padding;
+	let paddingContainer = window.getComputedStyle(posts__intro, null).padding;
 	let paddingBody = window.getComputedStyle(document.body, null).padding;
 	paddingBody = parseFloat(paddingBody.slice(4, 6));
 	paddingContainer = parseFloat(paddingContainer.slice(4, 6)*2);
 	if (paddingBody !== NaN) {
 		paddingContainer += paddingBody;
 	}
-	let widthContainer = container.offsetWidth - header__burger.offsetWidth - paddingContainer;
+	let widthContainer = posts__intro.offsetWidth - header__burger.offsetWidth;
+	if (document.body.offsetWidth >= 1300) {
+		widthContainer -= paddingContainer/2;
+	}
 	if (header__container.classList.contains('active')) {
 		header__burger.style.marginLeft = widthContainer + 'px';
 	} else {
 		header__burger.style.marginLeft = '0';
+		function showMessage() {
+			console.log(1);
+		}
+		header__burger.addEventListener("transitionend", showMessage());
 	}
 }
 
